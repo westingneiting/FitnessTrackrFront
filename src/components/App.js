c7aec5118d99dc163cbf9d503146d0a256ea23aa
Jason Williams <jwilliamstap@gmail.com>
2023-04-19 18:29:59 -0400

    app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+const nameArray = ["Jason", "Krystin", "Gyno", "Kevin", "Andrew"];
+
+
 function App() {
-  return <h1>APP!!!</h1>
+  return (
+    <div>
+      {
+        nameArray
+      }
+    </div>
+  )
 }
 
 export default App;

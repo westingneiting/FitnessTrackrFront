91e97b63644c3faf848dae73720d08066002f656
Westin <wgneiting1@gmail.com>
2023-06-15 17:23:39 -0600

    compnents index error fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,3 @@
 export { default as App } from './App';
 export { default as Register } from './Register';
-export { default as Posts } from './Posts';
-export { default as Login } from './Login';
-export { default as CreatePost } from './CreatePost';
-export { default as Nav } from './Nav';
-export { default as UpdatePost } from './UpdatePost';
-export { default as SendMessage } from './SendMessage';
-export { default as Profile } from './Profile';
 

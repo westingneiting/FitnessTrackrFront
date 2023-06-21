de31643fe24ce0b01b239960dc35d79e59807e18
Westin <wgneiting1@gmail.com>
2023-06-20 20:28:43 -0600

    activities searchbar example

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
 import { getAllActivities } from "../ajax-requests";
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, TextField } from '@mui/material';
 
 function Activities() {
   const [activities, setActivities] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     async function fetchActivities() {
@@ -14,18 +14,40 @@ function Activities() {
     fetchActivities();
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  function filterActivities(activities, searchQuery) {
+    return activities.filter(activity => activity.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  }
+
+  const filteredActivities = filterActivities(activities, searchQuery);
+
   return (
     <div>
       <Typography variant="h2" component="h2">Activities</Typography>
-      {activities.map((activity) => (
-        <Card key={activity.id}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
+      <TextField
+        variant="filled"
+        label="Search activities"
+        value={searchQuery}
+        onChange={handleSearchChange}
+        style={{ marginBottom: '1rem' }}
+        size='small'
+      />
+      {filteredActivities.length === 0 ? (
+        <p>No activities found</p>
+      ) : (
+        filteredActivities.map((activity) => (
+          <Card key={activity.id} style={{ marginBottom: '1rem' }}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
                 {activity.name}
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))
+      )}
     </div>
   )
 }

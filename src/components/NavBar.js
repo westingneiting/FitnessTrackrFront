e6ad5f8e475b6a9a6c25e1e6cad89a180cc7251d
Westin <wgneiting1@gmail.com>
2023-06-21 23:31:18 -0600

    small navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,132 +1,97 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Button, AppBar, Toolbar } from "@mui/material";
-import '../style.css'
+import "../style.css";
 
 const styles = {
   h1: {
-    marginRight: '25px'
+    marginRight: "auto",
   },
   appBar: {
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    backgroundColor: "rgba(100, 100, 100, 0.15)",
   },
   link: {
-    marginLeft: '1rem',
-    color: 'white', 
-    textDecoration: 'none'
-  },
-  toolbar: {
-    display: 'flex',
-    justifyContent: 'space-between'
+    color: "white",
+    textDecoration: "none",
+    minWidth: "100px",
   },
   container: {
-    display: 'flex',
-    alignItems: 'center',
-  
+    display: "flex",
+    alignItems: "center",
+    marginLeft: "auto",
   },
-  loginBtn: {
-    display: 'flex',
-    flexDirection: 'column',
-    
+  button: {
+    marginLeft: "5px",
   },
-  rgstrBtn: {
-    display: 'flex',
-    flexDirection: 'column',
-   
-  }
 };
 
 function NavBar({ setToken, setIsLoggedIn, isLoggedIn, navigate }) {
   const location = useLocation();
 
   function logout() {
-    setToken('');
+    setToken("");
     setIsLoggedIn(null);
-    window.localStorage.removeItem('token');
-    navigate('/login');
+    window.localStorage.removeItem("token");
+    navigate("/login");
   }
 
-  
-    return (
-      <AppBar position='static' style={styles.appBar}>
-        <Toolbar style={styles.toolbar}>
-          <h1 style={styles.h1}>Fitness Trackr</h1>
-          <div style={styles.container}>
-          <Button variant="outlined" style={{margin: '.5%'}}>
-            <Link to='/Routines'
-                style={{color: 'white', textDecoration: 'none'}}>
-                  Routines
-            </Link>
-          </Button>
-          <Button variant="outlined" style={{margin: '.5%', marginRight: '2%'}}>
-            <Link to='/Activities'
-                style={{color: 'white', textDecoration: 'none'}}>
-                  Activities
-            </Link>
-          </Button>
+  return (
+    <AppBar position="static" style={styles.appBar}>
+      <Toolbar>
+        <h1 style={styles.h1}>Fitness Trackr</h1>
+        <div style={styles.container}>
+          {location.pathname !== "/" && (
+            <Button variant="outlined" style={styles.button}>
+              <Link to="/" style={styles.link}>
+                Home
+              </Link>
+            </Button>
+          )}
+          {location.pathname !== "/routines" && (
+            <Button variant="outlined" style={styles.button}>
+              <Link to="/routines" style={styles.link}>
+                Routines
+              </Link>
+            </Button>
+          )}
+          {location.pathname !== "/activities" && (
+            <Button variant="outlined" style={styles.button}>
+              <Link to="/activities" style={styles.link}>
+                Activities
+              </Link>
+            </Button>
+          )}
           {isLoggedIn ? (
             <>
-            {location.pathname !== '/routines' && (
-              <Button>
-                <Link to='/create-post'
-                  style={styles.link}>
-                  Create Post
-                </Link>
-              </Button>
-            )}
-              {location.pathname !== '/activities' && (
-                <Button>
-                  <Link to='/profile'
-                    style={styles.link}>
-                    Profile
-                  </Link>
-                </Button>
-              )}
-              <Button onClick={logout}>
-                  <Link to='/login'
-                  style={styles.link}>
+              <Button onClick={logout} style={styles.button}>
+                <Link to="/login" style={styles.link}>
                   Log Out
-                  </Link>
+                </Link>
               </Button>
             </>
           ) : null}
           {!isLoggedIn ? (
             <>
-            {location.pathname !== '/' && (
-              <Button variant='outlined'>
-                <Link to='/'
-                  style={{color: 'white', textDecoration: 'none'}}>
-                  Home
-                </Link>
-              </Button>
-            )}
-            <div style={styles.loginBtn}>
-            {location.pathname !== '/login' && (
-              <>
-              <p style={{margin: '5%', }}>Already a memeber?</p>
-              <Button variant="outlined" style={{margin: '.5%'}}>
-                  <Link to='/login' 
-                  style={{color: 'white', textDecoration: 'none'}}>
+              {location.pathname !== "/login" && (
+                <Button variant="outlined" style={styles.button}>
+                  <Link to="/login" style={styles.link}>
                     Login
                   </Link>
-              </Button>
-              </>
-              )}</div>
-              <div style={styles.rgstrBtn}>
-              {location.pathname !== '/register' && (
-                <>
-                <p style={{margin: '5%', }}>Not a member?</p>
-              <Button variant="outlined" style={{margin: '.5%'}}>
-                  <Link to='/register' style={{color: 'white', textDecoration: 'none'}}>Register</Link>
-              </Button>
-              </>
-              )}</div>
+                </Button>
+              )}
+              {location.pathname !== "/register" && (
+                <Button variant="outlined" style={styles.button}>
+                  <Link to="/register" style={styles.link}>
+                    Register
+                  </Link>
+                </Button>
+              )}
             </>
           ) : null}
-          </div>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-   
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
 export default NavBar;
